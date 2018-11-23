a6b4073427502a71a7cc410ebebcadc0899eb768
Samuel Blattner <samworks@gmx.net>
2018-11-23 22:38:36 +0100

    Tidy up script

--- experiments/pir.py
+++ experiments/pir.py
@@ -1,11 +1,9 @@
 from RPi import GPIO
 import time
 from phue import Bridge
-from sense_hat import SenseHat
 
 b = Bridge('192.168.1.129')
-sense = SenseHat()
-b.set_group(0, {'on': True, 'bri': 254}, transitiontime=50)
+b.set_group(0, {'on': True, 'bri': 255}, transitiontime=50)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.IN)
@@ -15,10 +13,8 @@ try:
         if GPIO.input(16) == 0:
             b.set_group(0, {'on': False}, transitiontime=25)
         else:
-            b.set_group(0, {'on': True, 'bri': 254}, transitiontime=50)
+            b.set_group(0, {'on': True, 'bri': 255}, transitiontime=50)
         time.sleep(1)
-
-        print(sense.get_gyroscope_raw())
 
 except KeyboardInterrupt:
     print('Goodbye')
